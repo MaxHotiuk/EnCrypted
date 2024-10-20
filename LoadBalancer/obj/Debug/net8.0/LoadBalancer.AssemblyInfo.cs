81e77de8102b2388f74e324481692421099cf84a
bezshumu <103311303+MaxHotiuk@users.noreply.github.com>
2024-10-20 12:06:39 +0300

    ...

--- LoadBalancer/obj/Debug/net8.0/LoadBalancer.AssemblyInfo.cs
+++ LoadBalancer/obj/Debug/net8.0/LoadBalancer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoadBalancer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0515327910ba844c921e19fbbc60ca856e86f85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b30fd3693e3a768d62dedc2f35b00204016b10fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoadBalancer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoadBalancer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
